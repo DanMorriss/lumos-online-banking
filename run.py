c52a93e37fb16599511d29a35167f8bdefbda0ff
Daniel Morriss <danielmorriss1@gmail.com>
2023-07-28 16:04:07 +0100

    Add type function for delayed typing

--- run.py
+++ run.py
@@ -5,6 +5,9 @@ from google.oauth2.service_account import Credentials
 
 # Pyfiglet import for the logo
 import pyfiglet
+# Pythons time module for the staggered text
+from time import sleep
+import sys
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -21,5 +24,20 @@ customers = SHEET.worksheet('customers')
 
 data = customers.get_all_values()
 
+def type(text):
+    """
+    Types out the words letter by letter
+    """
+    sleep(0.5)
+    for letter in text:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        sleep(0.05)
+    print('')
+
+
 logo = pyfiglet.figlet_format('Lumos Online Banking')
 print(logo)
+sleep(0.5)
+type('Welcome to Lumos Online Banking')
+type('Please enter your unsername to login')
