cb9124e525473b08d6e19e81d1738a4114620fc9
Daniel Morriss <danielmorriss1@gmail.com>
2023-07-31 16:40:22 +0100

    Close while loops in login and create_account, start account_home

--- run.py
+++ run.py
@@ -44,15 +44,17 @@ def clear():
     Clears the terminal
     """
     os.system('clear')
+
+def print_logo():
+    LOGO = pyfiglet.figlet_format('Lumos Online Banking')
+    print(Fore.GREEN + LOGO)
     
 def welcome():
     """
     The welcome sequence asking if you would like to login or create an account.
     """
     # Display logo
-    LOGO = pyfiglet.figlet_format('Lumos Online Banking')
-    print(Fore.GREEN + LOGO)
-    sleep(0.5)
+    print_logo()
     # Welcome message
     type('Welcome to Lumos Online Banking')
     type('Would you like to login or create an account?')
@@ -61,13 +63,11 @@ def welcome():
     while True:
         existing_account = input(Fore.WHITE + '')
         if (existing_account == "1"):
-            type(Fore.GREEN + "Loading Login Page...")
-            sleep(0.5)
+            sleep(0.2)
             login()
             break
         elif (existing_account == "2"):
-            type(Fore.GREEN + 'Loading Account Setup...')
-            sleep(0.5)
+            sleep(0.2)
             create_account()
             break
         else:
@@ -87,7 +87,8 @@ def login():
     """
     customers_worksheet = SHEET.worksheet('customers')
     
-    while True:
+    login_loop = True
+    while login_loop:
         type(Fore.GREEN + 'Please enter your username to login')
         submitted_username = input(Fore.WHITE + '')
         # Check if username is in database
@@ -108,7 +109,9 @@ def login():
                 if (submitted_pin == user.pin):
                     pin_matched = True
                     type(Fore.GREEN + 'PIN correct, loading account details...')
-                    return user
+                    sleep(1)
+                    login_loop = False
+                    account_home(user.username, user.pin)
                 else:
                     type(Fore.GREEN + 'Incorrect PIN, please try again')
         else:
@@ -121,9 +124,10 @@ def create_account():
     with no white space and 
     A randomly generated 4 digit PIN and adds the information to the database.
     """
-    print('Please select a username between 5 and 15 characters long.')
+    print(Fore.GREEN + 'Please select a username between 5 and 15 characters long.')
     customer_database = SHEET.worksheet('customers')
-    while True:
+    account_loop = True
+    while account_loop:
         username = input(Fore.WHITE + '')
         # Don't allow repeat usernames
         if customer_database.find(username, in_column=1) is not None:
@@ -145,6 +149,7 @@ def create_account():
             print(Fore.BLUE + 'Account created.')
             print(f'Your PIN is: {created_user.pin}')
             # Run login function
+            account_loop = False
             login()
         else:
             print(Fore.GREEN + f'{username} is not valid. Please select a username between 5 and 10 characters long.')
@@ -152,6 +157,18 @@ def create_account():
 def generate_pin():
     pin = random.randint(1000,9999)
     return pin
+
+def account_home(username, pin):
+    clear()
+    print_logo()
+    type('Please select one of the following options:')
+    print('1: Check Account Blance')
+    print('2: Deposit Funds')
+    print('3: Withdraw Funds')
+    print('4: View PIN')
+    
+    print(username)
+    print(pin)
 
 def main():
     clear()
