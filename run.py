b860325821f2d9d6cb84725622d2d39e6f56fa8d
Daniel Morriss <danielmorriss1@gmail.com>
2023-07-31 21:09:48 +0100

    Fix pep8 errors

--- run.py
+++ run.py
@@ -28,6 +28,7 @@ SHEET = GSPREAD_CLIENT.open('lumos_online_banking')
 customers = SHEET.worksheet('customers')
 data = customers.get_all_values()
 
+
 def type(text):
     """
     Types out the words letter by letter
@@ -39,19 +40,22 @@ def type(text):
         sleep(0.05)
     print('')
 
+
 def clear():
     """
     Clears the terminal
     """
     os.system('clear')
 
+
 def print_logo():
     LOGO = pyfiglet.figlet_format('Lumos Online Banking')
     print(Fore.GREEN + LOGO)
-    
+
+
 def welcome():
     """
-    The welcome sequence asking if you would like to login or create an account.
+    The welcome sequence asking if you would like to login or create an account
     """
     # Display logo
     print_logo()
@@ -73,6 +77,7 @@ def welcome():
         else:
             print('Please enter 1 to Login or 2 to create an account')
 
+
 class User:
     """
     Creates a user class to store user information.
@@ -80,24 +85,25 @@ class User:
     def __init__(self, username, pin):
         self.username = username
         self.pin = pin
-            
+
+
 def login():
     """
     Checks database for username and validates the users PIN
     """
-    customers_worksheet = SHEET.worksheet('customers')
-    
+    cust_ws = SHEET.worksheet('customers')
+
     login_loop = True
     while login_loop:
         type(Fore.GREEN + 'Please enter your username to login')
         submitted_username = input(Fore.WHITE + '')
         # Check if username is in database
-        stored_username = customers_worksheet.find(submitted_username, in_column=1)
-        
+        stored_un = cust_ws.find(submitted_username, in_column=1)
+
         # Check PIN if username in database or ask for valid username
-        if stored_username:
+        if stored_un:
             # Get PIN associated with username
-            stored_pin = customers_worksheet.cell(stored_username.row, stored_username.col + 1).value
+            stored_pin = cust_ws.cell(stored_un.row, stored_un.col + 1).value
             # Create user object to store username and pin
             user = User(submitted_username, stored_pin)
             type(Fore.GREEN + f'Welcome {user.username}')
@@ -108,33 +114,33 @@ def login():
                 submitted_pin = input(Fore.WHITE + '')
                 if (submitted_pin == user.pin):
                     pin_matched = True
-                    type(Fore.GREEN + 'PIN correct, loading account details...')
+                    type(Fore.GREEN + 'PIN correct, loading account details..')
                     sleep(1)
                     login_loop = False
                     account_home(user.username, user.pin)
                 else:
-                    type(Fore.GREEN + 'Incorrect PIN, please try again')
+                    print(Fore.GREEN + 'Incorrect PIN, please try again')
         else:
-            type(Fore.GREEN + 'User not found, please try again.')  
-    
-    
+            print(Fore.GREEN + 'User not found, please try again.')
+
+
 def create_account():
     """
-    Create an account with a user generate username between 5 and 15 characters long
-    with no white space and 
+    Create an account with a user generated username between 5 and 15
+    characters long with no white space and
     A randomly generated 4 digit PIN and adds the information to the database.
     """
-    print(Fore.GREEN + 'Please select a username between 5 and 15 characters long.')
+    print(Fore.GREEN + 'Select a username between 5 and 15 characters long.')
     customer_database = SHEET.worksheet('customers')
     account_loop = True
     while account_loop:
         username = input(Fore.WHITE + '')
         # Don't allow repeat usernames
         if customer_database.find(username, in_column=1) is not None:
-            print(Fore.GREEN + 'Username not avalilable, please choose a different one.')
+            print(Fore.GREEN + 'Username unavalible, try again.')
         # Check for whitespace
         elif (any(char.isspace() for char in username)):
-            print(Fore.GREEN + f'{username} is not valid. Please pick a username without any spaces.') 
+            print(Fore.GREEN + f'{username} not valid, containes whitespace.')
         # Make username between 5 and 15 characters
         elif (len(username) < 16) and (len(username) > 4):
             print(Fore.GREEN + f'{username} is a valid username')
@@ -152,26 +158,31 @@ def create_account():
             account_loop = False
             login()
         else:
-            print(Fore.GREEN + f'{username} is not valid. Please select a username between 5 and 10 characters long.')
+            print(Fore.GREEN + f'{username} is not valid.')
+            print('Select a username between 5 and 10 characters long.')
+
 
 def generate_pin():
-    pin = random.randint(1000,9999)
+    pin = random.randint(1000, 9999)
     return pin
+
 
 def account_home(username, pin):
     clear()
     print_logo()
+    type(f'Welcome {username}')
     type('Please select one of the following options:')
     print('1: Check Account Blance')
     print('2: Deposit Funds')
     print('3: Withdraw Funds')
     print('4: View PIN')
-    
     print(username)
     print(pin)
+
 
 def main():
     clear()
     welcome()
-    
+
+
 main()
